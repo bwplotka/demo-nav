63b0229a3eb8d92f8d5ce0d795990e38ba15ad8a
Bartek Plotka <bwplotka@gmail.com>
2019-02-10 14:33:09 +0000

    Shellcheck initial structs.
    Signed-off-by: Bartek Plotka <bwplotka@gmail.com>

--- shelltest/src/lib.rs
+++ shelltest/src/lib.rs
@@ -1,11 +1,41 @@
-pub fn test(){
-    println!("lol")
+use std::io;
+use std::fs::File;
+
+pub enum Shell {
+    Current,
+}
+
+pub struct Suite {
+   shell: Shell,
+   script: String
+}
+
+impl Suite {
+    fn setup(filename: &str) -> io::Result<Suite> {
+        match File::open(filename)  {
+            Err(err) => return Err(err),
+            _ => (),
+        }
+
+        Ok(Suite {
+            shell: Shell::Current,
+            script: String::from(filename),
+        })
+    }
+}
+
+impl Drop for Suite {
+    fn drop(&mut self) {
+        println!("Teardown Suite with data `{}`!", self.script)
+    }
 }
 
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
+    fn outputs() {
+        let mut s = super::Suite::setup("./testdata/script1.sh");
+
         assert_eq!(2 + 2, 4);
     }
 }
