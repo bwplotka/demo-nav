c47d9b08cc5bb7d1867465953abce383d5dece21
Bartek Plotka <bwplotka@gmail.com>
2019-02-10 17:12:27 +0000

    Somde stuff.
    Signed-off-by: Bartek Plotka <bwplotka@gmail.com>

--- shelltest/src/lib.rs
+++ shelltest/src/lib.rs
@@ -1,41 +1,101 @@
-use std::io;
 use std::fs::File;
+use std::thread;
+use std::process::{Child, Command, Stdio};
 
 pub enum Shell {
     Current,
 }
 
+pub enum Handle {
+    Stdout,
+    Stderr,
+    File(String),
+}
+
 pub struct Suite {
-   shell: Shell,
-   script: String
+    shell: Shell,
+    script: String,
+    args: Vec<String>,
+
+    child: Option<Child>,
+    handle: thread::JoinHandle<Send + 'static {Builder::new().spawn(f).unwrap() }>,
+}
+
+pub fn new(script: &str) -> Suite {
+    File::open(script).expect(
+        format!(
+            r#"assertion failed: `script {} being present`
+    >>"#,
+            script
+        )
+        .as_str(),
+    );
+
+    Suite {
+        shell: Shell::Current,
+        script: String::from(script),
+        args: [].to_vec(),
+
+        child: None,
+        handle: thread::JoinHandle,
+    }
 }
 
 impl Suite {
-    fn setup(filename: &str) -> io::Result<Suite> {
-        match File::open(filename)  {
-            Err(err) => return Err(err),
-            _ => (),
+    pub fn expect(mut self, handle: Handle, s: &str) -> Suite {
+        if let None = self.child {
+            self.exec();
         }
 
-        Ok(Suite {
-            shell: Shell::Current,
-            script: String::from(filename),
-        })
+        self
+    }
+
+    fn exec(&mut self) {
+        let handle = thread::spawn(|| {
+            for i in 1..10 {
+                println!("hi number {} from the spawned thread!", i);
+                thread::sleep(Duration::from_millis(1));
+            }
+        });
+        for i in 1..5 {
+            println!("hi number {} from the main thread!", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+        handle.join().unwrap();
+
+
+
+
+
+        self.child = Some(Command::new(self.script.clone())
+            .args(self.args.clone())
+            .stderr(Stdio::from())
+            .
+            .spawn()
+            .expect(
+                format!(
+                    r#"assertion failed: `cmd {} exec failed`
+    >>"#,
+                    self.script.as_str()
+                )
+                .as_str(),
+            ));
+
+        self.child.expect("").
     }
 }
 
 impl Drop for Suite {
-    fn drop(&mut self) {
-        println!("Teardown Suite with data `{}`!", self.script)
-    }
+    fn drop(&mut self) {}
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn outputs() {
-        let mut s = super::Suite::setup("./testdata/script1.sh");
-
-        assert_eq!(2 + 2, 4);
+        new("./src/testdata/script1.sh")
+            .expect(Handle::Stdout, "test output");
     }
 }
