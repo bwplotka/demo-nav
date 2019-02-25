4e237141463bb7d7460d0f3d84c1a2f4ef2f4f71
Bartek Plotka <bwplotka@gmail.com>
2019-02-25 07:40:52 +0000

    Removed RUST tests for now. Will add this in another PR.
    Signed-off-by: Bartek Plotka <bwplotka@gmail.com>

--- shelltest/src/testdata/script1.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-echo "test output"
-
-echo "test err ouput" > /dev/stderr
--- tests/src/lib.rs
+++
@@ -1,8 +0,0 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        shelltest::test();
-        assert_eq!(2 + 2, 4);
-    }
-}
